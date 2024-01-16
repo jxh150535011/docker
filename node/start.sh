39bb0223067d071e3face0e4146c46dad42fabb8
xingheng.jin <xingheng.jin@nio.com>
2024-01-16 18:57:28 +0800

    基于musl libc 的代码版本

--- node/start.sh
+++ node/start.sh
@@ -7,6 +7,19 @@ ids=$(docker ps -a -f ancestor=$name:$tag -q)
 if [ $ids ] ; then
   docker rm -f $ids
 fi
-docker rmi -f $name:$tag
+# 如果不增加 每次改动 都会生成新的镜像 但是先进行了删除会导致镜像构建无法有效利用缓存
+# docker rmi -f $name:$tag
+
+oimageid=$(docker images -q --filter=reference="$name:$tag" | head -n 1)
+# | tee build.log
 docker build -f ./Dockerfile  -t $name:$tag .
+nimageid=$(docker images -q --filter=reference="$name:$tag" | head -n 1)
+
+# # 所以采用先获取到上次的镜像id 
+if [ $oimageid ] && [ $oimageid != $nimageid ] ; then
+  docker rmi -f $oimageid
+fi
+
 docker run -it --name $name -d $name:$tag
+
+
